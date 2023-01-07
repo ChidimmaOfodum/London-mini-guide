4199775dc0d5ef60573b33fce4849a9dfbb29795
Chidimma <chidimmamofodum@gmail.com>
2023-01-07 19:54:44 +0000

    defined dynamic route for city and locations

--- server.js
+++ server.js
@@ -5,39 +5,55 @@ const port = 3000;
 //....................reading files.............................
 
 // const harrow = JSON.parse(fs.readFileSync(`${__dirname}/data/Harrow.json`, "utf8"))
-const harrow = require("./data/Harrow.json")
-const heathrow = require("./data/Heathrow.json")
-const stratford = require("./data/Stratford.json")
+const harrow = require("./data/Harrow.json");
+const heathrow = require("./data/Heathrow.json");
+const stratford = require("./data/Stratford.json");
 
+const data = { harrow: harrow, heathrow: heathrow, stratford: stratford };
 
 //..........defining routes........................................................
-let {pharmacies, colleges, hospitals, doctors} = stratford
+let { pharmacies, colleges, hospitals, doctors } = stratford;
 
 app.get("/pharmacies", (req, res) => {
-    res.json(pharmacies)
-})
+  res.json(pharmacies);
+});
 
 app.get("/colleges", (req, res) => {
   res.json(colleges);
 });
 
 app.get("/hospitals", (req, res) => {
-    res.json(hospitals)
-   ;
+  res.json(hospitals);
 });
 
 app.get("/doctors", (req, res) => {
-    res.json(doctors);
+  res.json(doctors);
+});
+
+// routes to get data in all cities
+
+// :/city/:category
+
+app.get("/:city", (req, res) => {
+  let city = req.params.city.toLowerCase();
+  res.send(data[city]);
+});
+
+app.get("/:city/:place", (req, res) => {
+  const city = req.params.city.toLowerCase();
+  const place = req.params.place.toLowerCase();
+  const result = data[city][place];
+  if(result) {
+    res.json(result)
+  } else {
+    res.status(404).json({
+        message: "Not found"
+    })
+  }
 });
 
 //.......................Starting Server....................................
 
 app.listen(port, () => {
-    console.log("Listening on port " + port)
-})
-
-
-
-
-
-
+  console.log("Listening on port " + port);
+});
